0a7c050d973a875944dcc06b59d3ba59a6dc4173
mahfujur <xmahfujur@gmail.com>
2026-05-12 15:46:43 +0600

    add all dependency

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import os
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = '0.0.0'
+
+REPO_NAME = "Chest-Cancer-Classification-using-MLflow"
+AUTHOR_USER_NAME = 'xmahfujur'
+SRC_REPO =  'cnnClassifier'
+AUTHOR_EMAIL = 'xmahfujur@gmail.com'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description='A small python package for cnn app',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}',
+    project_urls={
+        'Bug Tracker': f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues'
+    },
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
